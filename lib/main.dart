0c629d0727637d39f5bfdffebd58b1f206a06df8
Alexandre Assunção <apprendiiz@yahoo.com.br>
2023-10-22 20:19:10 -0300

    feat: ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,301 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const back4AppBaseUrl = 'https://parseapi.back4app.com/classes/ceps';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _cepController = TextEditingController();
+  String _errorMessage = "";
+  String _cepResponse = "";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Consulta de CEP'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            TextField(
+              controller: _cepController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'CEP (Formato: 00000000)'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: () async {
+                final cep = _cepController.text;
+
+                if (isValidCep(cep)) {
+                  final viaCepUrl = 'https://viacep.com.br/ws/$cep/json/';
+                  final response = await http.get(Uri.parse(viaCepUrl));
+
+                  if (response.statusCode == 200) {
+                    final data = json.decode(response.body);
+                    _cepResponse =
+                        "Cidade: ${data['localidade']}, UF: ${data['uf']}";
+                    sendCepToBack4App(cep, data);
+                  } else {
+                    _cepResponse = "CEP não encontrado";
+                  }
+                } else {
+                  _cepResponse = "CEP inválido";
+                }
+
+                setState(() {});
+              },
+              child: Text('Consultar e cadastrar CEP'),
+            ),
+            Text(_errorMessage, style: TextStyle(color: Colors.red)),
+            Text(_cepResponse),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ListCepsScreen()),
+                );
+              },
+              child: Text('Listar CEPs'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  bool isValidCep(String cep) {
+    final cepRegExp = RegExp(r'^\d{8}$');
+    return cepRegExp.hasMatch(cep);
+  }
+
+  Future<void> sendCepToBack4App(
+      String cep, Map<String, dynamic> viaCepData) async {
+    final url = Uri.parse(back4AppBaseUrl);
+    final response = await http.post(
+      url,
+      headers: {
+        'X-Parse-Application-Id': '', //virá do .env
+        'X-Parse-REST-API-Key': '', //virá do .env
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'cep': cep,
+        'city': viaCepData['localidade'],
+        'state': viaCepData['uf'],
+      }),
+    );
+
+    if (response.statusCode == 201) {
+    } else {
+      print('Erro ao enviar CEP para o Back4App: ${response.body}');
+    }
+  }
+}
+
+class ListCepsScreen extends StatefulWidget {
+  @override
+  _ListCepsScreenState createState() => _ListCepsScreenState();
+}
+
+class _ListCepsScreenState extends State<ListCepsScreen> {
+  List<Map<String, dynamic>> ceps = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCepsFromBack4App();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de CEPs'),
+      ),
+      body: ListView.builder(
+        itemCount: ceps.length,
+        itemBuilder: (context, index) {
+          final cep = ceps[index];
+          return ListTile(
+            title: Text('CEP: ${cep['cep']}'),
+            subtitle: Text('Cidade: ${cep['city']}, UF: ${cep['state']}'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditCepScreen(cep),
+                      ),
+                    );
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    deleteCepFromBack4App(cep['objectId']);
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Future<void> fetchCepsFromBack4App() async {
+    final url = Uri.parse(back4AppBaseUrl);
+    final response = await http.get(
+      url,
+      headers: {
+        'X-Parse-Application-Id': '', //virá do .env
+        'X-Parse-REST-API-Key': '', //virá do .env
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      ceps = List<Map<String, dynamic>>.from(data['results']);
+      setState(() {});
+    } else {
+      print('Erro ao buscar CEPs do Back4App: ${response.body}');
+    }
+  }
+
+  Future<void> deleteCepFromBack4App(String objectId) async {
+    final url = Uri.parse('$back4AppBaseUrl/$objectId');
+    final response = await http.delete(
+      url,
+      headers: {
+        'X-Parse-Application-Id': '', //virá do .env
+        'X-Parse-REST-API-Key': '', //virá do .env
+      },
+    );
+
+    if (response.statusCode == 200) {
+      fetchCepsFromBack4App();
+    } else {
+      print('Erro ao excluir CEP do Back4App: ${response.body}');
+    }
+  }
+}
+
+class EditCepScreen extends StatefulWidget {
+  final Map<String, dynamic> cep;
+
+  EditCepScreen(this.cep);
+
+  @override
+  _EditCepScreenState createState() => _EditCepScreenState();
+}
+
+class _EditCepScreenState extends State<EditCepScreen> {
+  final TextEditingController _cepController = TextEditingController();
+  final TextEditingController _cityController = TextEditingController();
+  final TextEditingController _stateController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _cepController.text = widget.cep['cep'];
+    _cityController.text = widget.cep['city'];
+    _stateController.text = widget.cep['state'];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Editar CEP'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _cepController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'CEP (Formato: 00000000)'),
+            ),
+            TextField(
+              controller: _cityController,
+              decoration: InputDecoration(labelText: 'Cidade'),
+            ),
+            TextField(
+              controller: _stateController,
+              decoration: InputDecoration(labelText: 'Estado'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final cep = _cepController.text;
+                final city = _cityController.text;
+                final state = _stateController.text;
+
+                if (isValidCep(cep)) {
+                  updateCepInBack4App(widget.cep['objectId'], cep, city, state);
+                } else {}
+              },
+              child: Text('Salvar Alterações'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  bool isValidCep(String cep) {
+    final cepRegExp = RegExp(r'^\d{8}$');
+    return cepRegExp.hasMatch(cep);
+  }
+
+  Future<void> updateCepInBack4App(
+      String objectId, String cep, String city, String state) async {
+    final url = Uri.parse('$back4AppBaseUrl/$objectId');
+    final response = await http.put(
+      url,
+      headers: {
+        'X-Parse-Application-Id': '', //virá do .env
+        'X-Parse-REST-API-Key': '', //virá do .env
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'cep': cep,
+        'city': city,
+        'state': state,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => ListCepsScreen()),
+      );
+    } else {
+      print('Erro ao atualizar CEP no Back4App: ${response.body}');
+    }
   }
 }
